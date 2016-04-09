a1a7dee9fa500c6de1f944c0bd030c660f48154b
Lucas <lucas@agtlucas.com>
2016-04-09 20:53:30 -0300

    Update App.js

---
+++ app/App.js
@@ -0,0 +1,23 @@
+import React, {
+  View,
+  Text,
+  Component,
+  TextInput,
+  StyleSheet,
+  TouchableHighlight,
+  ScrollView
+} from 'react-native'
+
+import Realm from 'realm'
+import _ from 'lodash'
+
+let realm = new Realm({
+  schema: [{
+    name: 'Categories',
+    'properties': {
+      name: 'string'
+    }
+  }]
+})
+
+let favs = realm.objects('Categories')
