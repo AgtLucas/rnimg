2fe1733ca77baa092e8d47989df1d5b06d707d79
Lucas <lucas@agtlucas.com>
2016-04-10 06:54:04 -0300

    Update App.js

--- app/App.js
+++ app/App.js
@@ -11,6 +11,64 @@ import React, {
 import Realm from 'realm'
 import _ from 'lodash'
 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
+  _closeModal () {
+    this.props.navigator.pop()
+  }
+
+  _updateInput (input) {
+    this.setState({ input })
+  }
+
+  _addItem () {
+    if (this.state.input === '') return
+    realm.write(() => {
+      realm.create('Categories', { name: this.state.input })
+    })
+    this.setState({ input: '' })
+  }
+
+  _deleteItem (item) {
+    let itemToDelete = favs.filtered('name = $0', name)
+    realm.write(() => {
+      realm.delete(itemToDelete)
+    })
+    this.forceUpdate()
+  }
+
+  _viewImages (category) {
+    this.props.navigator.push({
+      name: 'ViewImages',
+      passProps: {
+        closeModal: this._closeModal,
+        category
+      }
+    })
+  }
+
+  render () {
+    let favorites = _.map(favs, (f, i) => {
+      return (
+        <View key={i} style={style.favoriteButtonContainer}>
+          <TouchableHighlight
+            onPress={() => this._viewImages(f.name)}
+            underlayColor='transparent'
+            style={style.favorite}>
+            <Text style={style.favoriteText}>{f.name}</Text>
+          </TouchableHighlight>
+        </View>
+      )
+    })
+  }
+}
+
 let realm = new Realm({
   schema: [{
     name: 'Categories',
@@ -21,19 +79,3 @@ let realm = new Realm({
 })
 
 let favs = realm.objects('Categories')
-
-_addItem () {
-  if (this.state.input === '') return
-  realm.write(() => {
-    realm.create('Categories', { name: this.state.input })
-  })
-  this.setState({ input: '' })
-}
-
-_deleteItem (item) {
-  let itemToDelete = favs.filtered('name = $0', name)
-  realm.write(() => {
-    realm.delete(itemToDelete)
-  })
-  this.forceUpdate()
-}
