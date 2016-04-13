67a619081b3bba29ed667a380873978114eaef27
Lucas <lucas@agtlucas.com>
2016-04-12 21:55:30 -0300

    Fix typo on index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,7 @@ import React, {
 } from 'react-native'
 
 import App from './app/App'
-import ViewImages from './app.ViewImages'
+import ViewImages from './app/ViewImages'
 
 class RNImg extends Component {
   renderScene (route, navigator) {
